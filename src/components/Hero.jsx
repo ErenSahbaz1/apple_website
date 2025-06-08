501a937defd369d94db2761c96c4180bb91d809d
Eren Sahbaz <eren.sahbaz@student.ehb.be>
2025-06-08 11:44:00 +0200

    feat: add GSAP for animations in Hero component and update dependencies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 const Hero = () => {
+	useGSAP(() => {
+		gsap.to("#hero", {
+			opacity: 1,
+			delay: 1.5,
+		});
+	}, []);
+
 	return (
 		<section className="w-full nav-height bg-black relative">
 			<div className="flex h-5/6 w-full flex-center flex-col">
-				<p>iPhone 15 pro</p>
+				<p id="hero" className="hero-title">
+					iPhone 15 pro
+				</p>
 			</div>
 		</section>
 	);
